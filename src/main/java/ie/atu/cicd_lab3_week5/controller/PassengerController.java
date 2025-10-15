5c7d510749814a17b2dfeae9addbb759b1b64490
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-10-15 14:23:17 +0100

    update feature branch locally

--- src/main/java/ie/atu/cicd_lab3_week5/controller/PassengerController.java
+++ src/main/java/ie/atu/cicd_lab3_week5/controller/PassengerController.java
@@ -4,10 +4,7 @@ package ie.atu.cicd_lab3_week5.controller;
 import ie.atu.cicd_lab3_week5.model.Passenger;
 import ie.atu.cicd_lab3_week5.service.PassengerService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,10 +15,23 @@ public class PassengerController {
 
     private final PassengerService service;             //constructor DI
 
-    public PassengerController(PassengerService service) { this.service = service; }
+    public PassengerController(PassengerService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Passenger>> getAll() { return ResponseEntity.ok(service.findAll()); }
 
     @GetMapping("/{id}")
-    public ResponseEntity<List<Passenger>> getAll() { return ResponseEntity.ok(service.findAll()) {
+    public ResponseEntity<Passenger> getOne(@PathVariable String id) {
+        Optional<Passenger> maybe = service.findById(id);
+        if (maybe.isPresent()) {
+            return ResponseEntity.ok(maybe.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
 
+        @PostMapping
+                public ResponseEntity
     }
 }
