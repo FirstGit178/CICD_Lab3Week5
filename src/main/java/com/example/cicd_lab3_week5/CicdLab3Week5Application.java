60a8b8c6ec3ba9bb4a1bcc36c26d7451b2f658f2
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-10-15 12:51:45 +0100

    Initial Commit

---
+++ src/main/java/com/example/cicd_lab3_week5/CicdLab3Week5Application.java
@@ -0,0 +1,13 @@
+package com.example.cicd_lab3_week5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLab3Week5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLab3Week5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd_lab3_week5/CicdLab3Week5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_lab3_week5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLab3Week5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
