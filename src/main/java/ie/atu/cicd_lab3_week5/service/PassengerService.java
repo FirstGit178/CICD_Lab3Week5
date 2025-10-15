b9adceb2de0c4ca7173efdb484b5d2e49196fb87
FergalMcNamara-STUDE <g00180082@atu.ie>
2025-10-15 13:38:18 +0100

    PassengerService

--- src/main/java/ie/atu/cicd_lab3_week5/service/PassengerService.java
+++ src/main/java/ie/atu/cicd_lab3_week5/service/PassengerService.java
@@ -1,4 +1,34 @@
 package ie.atu.cicd_lab3_week5.service;
 
+import ie.atu.cicd_lab3_week5.model.Passenger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class PassengerService {
+    private final List<Passenger> store = new ArrayList<>();
+
+    public List<Passenger> findAll() {
+        return new ArrayList<>(store);  //defensive copy
+
+    }
+
+    public Optional<Passenger> findById(String id) {
+        for (Passenger p : store) {
+            if (p.getPassengerId().equals(id)) {
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
+
+    }
+
+    public Passenger create(Passenger p) {
+        if (findById(p.getPassengerId()).isPresent()) {
+            throw new IllegalArgumentException("passengerId already exists");
+        }
+        store.add(p);
+        return p;
+    }
 }
